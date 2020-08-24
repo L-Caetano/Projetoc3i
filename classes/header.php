bb53ce365ee052e1b63a7329289c3bee6c12bb12
Lucas Caetano <lucas.caetanov2013@gmail.com>
2020-08-24 18:45:06 -0300

    Novo Header

--- classes/header.php
+++ classes/header.php
@@ -11,10 +11,11 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<nav class="navbar navbar-default ">
   <?php
 include_once '../css/custom.css';
+
 ?>
+<nav class="navbar navbar-default ">
   <div class="container-fluid ">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -30,13 +31,26 @@ include_once '../css/custom.css';
         <li><img src="../css/favicon.ICO"></li>
         <?php
         echo '
+
         <li><a href="../novos/novo_usuario.php">Registrar Novo Usuario</a></li>
         ';
         if((isset($_SESSION['login']))and($_SESSION['login'] == true)){
-echo '<li><a  href="../listar/listar_computadores.php">Lista de Materiais</a></li>
-    <li><a  href="../novos/novo_computador.php">Novo Material</a></li>
-    <li><a  href="../novos/novo_formulario.php">Nova Ordem</a></li>
-    <li><a  href="../listar/listar_formularios.php">Ordens de serviço</a></li>
+echo '<li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Listar
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="../listar/listar_computadores.php">Materiais</a></li>
+          <li><a href="../listar/listar_formularios.php">Ordens de serviço</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Novo(a)
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="../novos/novo_computador.php">Materiais</a></li>
+          <li><a href="../novos/novo_formulario.php">Ordem de serviço</a></li>
+        </ul>
+      </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="delogarteste.php"><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
@@ -57,4 +71,5 @@ echo '<li><a  href="../listar/listar_computadores.php">Lista de Materiais</a></l
 </nav>';
 }
 
+?>
 
